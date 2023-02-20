00c112669e5727c38873cd5a59c84495685f8233
renbaba1997 <592649842@qq.com>
2023-02-20 19:43:27 +0800

    first commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { testInit } from '@/api/initTest'
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const params = {
+        name: '小明',
+        age: 25,
+        city: '六安'
+      }
+      testInit(params)
+        .then(response => {
+          debugger
+          console.log(response.data)
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
