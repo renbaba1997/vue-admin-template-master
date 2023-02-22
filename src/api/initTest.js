48f218d2a759cdd0cdfee58db9b3a05a108a2455
renbaba1997 <592649842@qq.com>
2023-02-22 19:31:14 +0800

    22

--- src/api/initTest.js
+++ src/api/initTest.js
@@ -7,3 +7,27 @@ export function testInit(data) {
     data
   })
 }
+
+export function addStudentInfo(data) {
+  return request({
+    url: '/addStudentInfo',
+    method: 'post',
+    data
+  })
+}
+
+export function deleteStudentInfo(data) {
+  return request({
+    url: '/deleteStudentInfo',
+    method: 'post',
+    data
+  })
+}
+
+export function deleteStudentInfos(data) {
+  return request({
+    url: '/deleteStudentInfos',
+    method: 'post',
+    data
+  })
+}
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -40,13 +40,13 @@
         </el-col>
       </el-row>
       <el-row>
-        <el-table :data="tableData" tooltip-effect="dark" stripe style="margin-top: 20px; width: 100%;" @selection-change="handleSelectionChange" row-key="studentNo" border>
+        <el-table :data="tableData" tooltip-effect="dark" stripe style="margin-top: 20px; width: 100%;" @selection-change="handleSelectionChange" row-key="studentNo">
           <el-table-column type="selection" width="50px" height="50px" :selectable="checkBoxState" :reserve-selection="true"></el-table-column>
-          <el-table-column fixed="left" key="1" width="auto" min-width="5%" label="序号" align="center">
-            <template scope="scope">
-              <span>{{(pageNum - 1) * pageSize + scope.$index + 1 }}</span>
-            </template>
-          </el-table-column>
+            <el-table-column fixed="left" key="1" width="auto" min-width="5%" label="序号" align="center">
+              <template scope="scope">
+                <span>{{(pageNum - 1) * pageSize + scope.$index + 1 }}</span>
+              </template>
+            </el-table-column>
           <el-table-column key="2" label="学生编号" prop="studentNo" width="auto" min-width="15%" align="center"></el-table-column>
           <el-table-column key="3" label="学生姓名" prop="name" width="auto" min-width="15%" align="center"></el-table-column>
           <el-table-column key="4" label="学生年龄" prop="age" width="auto" min-width="15%" align="center"></el-table-column>
@@ -59,15 +59,47 @@
           </el-table-column>
         </el-table>
         <div style="margin-top: 10px;">
-          <el-pagination style="text-align: center;" :current-change="currentChange" @size-change="sizeChange" :current-page="pageNum" :page-size="pageSize" layout="prev, pager, next, total" :total="total" background @current-change="currentChange"></el-pagination>
+          <el-pagination style="text-align:center;" :current-change="currentChange" @size-change="sizeChange" :current-page="pageNum" :page-size="pageSize" layout="prev, pager, next, total" :total="total" background @current-change="currentChange"></el-pagination>
         </div>
       </el-row>
     </el-form>
+    <el-dialog title="新增学生信息" :visible.sync="dialogVsible" width="45%" :show-close="false" center>
+      <el-form :model="addForm" label-width="100px" label-position="left" :rules="addFormRules" ref="addFormRef" style="margin-left: 40px;">
+        <el-row>
+          <el-col :span="10">
+            <el-form-item label="学号" prop="studentNo">
+              <el-input v-model="addForm.studentNo" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="10">
+            <el-form-item label="姓名" prop="name">
+              <el-input v-model="addForm.name" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="10">
+            <el-form-item label="年龄" prop="age">
+              <el-input v-model="addForm.age" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span="10">
+            <el-form-item label="城市" prop="city">
+              <el-input v-model="addForm.city" style="width: 150px;"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancelInput(addForm)" icon="el-icon-circle-close">取消</el-button>
+        <el-button type="primary" @click="addRow(addForm)" icon="el-icon-circle-check">提交</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { testInit } from '@/api/initTest'
+import { testInit, addStudentInfo, deleteStudentInfo, deleteStudentInfos } from '@/api/initTest'
 export default {
   data() {
     return {
@@ -77,10 +109,18 @@ export default {
         age: '',
         city: ''
       },
+      addForm: {},
       tableData: [],
+      dialogVsible: false,
+      addFormRules: {
+        studentNo: [{ required: true, message: '请输入学生编号', trigger: 'blur' }],
+        name: [{ required: true, message: '请输入学生姓名', trigger: 'blur' }],
+        age: [{ required: true, message: '请输入学生年龄', trigger: 'blur' }],
+        city: [{ required: true, message: '请输入学生所在城市', trigger: 'blur' }]
+      },
       multipleSelection: [],
       pageNum: 1,
-      pageSize: 6,
+      pageSize: 8,
       total: 4
     }
   },
@@ -93,6 +133,19 @@ export default {
     onInput() {
       this.$forceUpdate()
     },
+    cancelInput(addForm) {
+      this.addForm.studentNo = ''
+      this.addForm.name = ''
+      this.addForm.age = ''
+      this.addForm.city = ''
+      this.dialogVsible = false
+    },
+    reset() {
+      this.searchForm.studentNo = ''
+      this.searchForm.name = ''
+      this.searchForm.age = ''
+      this.searchForm.city = ''
+    },
     currentChange(currentChange) {
       this.pageNum = currentChange
       this.initTableData()
@@ -104,6 +157,7 @@ export default {
     handleSelectionChange(val) {
       this.multipleSelection = val
     },
+    // 初始化
     async initTableData() {
       const params = {
         pageIndex: this.pageNum,
@@ -111,12 +165,97 @@ export default {
       }
       testInit(params)
         .then(response => {
-          debugger
           this.tableData = response.data.data
           this.total = response.data.total
         })
         .catch(error => {
           console.log(error)
+        })
+    },
+    // 查询函数
+    async search() {
+      const params = {
+        studentNo: this.searchForm.studentNo,
+        name: this.searchForm.name,
+        age: this.searchForm.age,
+        city: this.searchForm.city
+      }
+      testInit(params)
+        .then(response => {
+          this.tableData = response.data.data
+          this.total = response.data.total
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    // 增加学生信息
+    addRow(addForm) {
+      this.pageNum = 1
+      this.$refs.addFormRef.validate(valid => {
+        if (!valid) return this.$message.warning('表单尚未完全填写')
+        const params = {
+          studentNo: this.addForm.studentNo,
+          name: this.addForm.name,
+          age: this.addForm.age,
+          city: this.addForm.city
+        }
+        this.addForm.studentNo = ''
+        this.addForm.name = ''
+        this.addForm.age = ''
+        this.addForm.city = ''
+        debugger
+        addStudentInfo(params)
+          .then(response => {
+            debugger
+            this.initTableData()
+            this.dialogVsible = false
+          })
+          .catch(error => {
+            this.$message.error(error.message)
+          })
+      })
+    },
+    // 删除
+    deleteRow(row) {
+      this.$confirm('确定要删除选中的数据吗？', '系统提示', {
+        cancelButtonText: '取消',
+        confirmButtonText: '确定',
+        type: 'warning'
+      })
+        .then(() => {
+          const params = {
+            studentNo: row.studentNo
+          }
+          deleteStudentInfo(params)
+            .then(response => {
+              this.pageNum = 1
+              this.initTableData()
+              this.dialogVsible = false
+            })
+            .catch(error => {
+              this.$message.error(error.message)
+            })
+        })
+    },
+    delayMany() {
+      this.$confirm('确定要删除选中的数据吗？', '系统提示', {
+        cancelButtonText: '取消',
+        confirmButtonText: '确定',
+        type: 'warning'
+      })
+        .then(() => {
+          const params = [...this.multipleSelection]
+          deleteStudentInfos(params)
+            .then(response => {
+              this.pageNum = 1
+              this.initTableData()
+              this.dialogVsible = false
+              alert('删除成功！')
+            })
+            .catch(error => {
+              this.$message.error(error.message)
+            })
         })
     }
   }
